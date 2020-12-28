5c5576627abdfd5c9aa4a29a0abd9b233c81d7d3
Vega Julián <70970180+julivega93@users.noreply.github.com>
2020-12-28 16:33:08 -0300

    Update paneladm.php

--- application/views/paneladm.php
+++ application/views/paneladm.php
@@ -1,8 +1,4 @@
-<div class="container container-fluid">
 
-
-    
-    
     <div class="bg-dark rounded mt-3" style="height:220px">
     <ul class="nav flex-column">
       <li class="nav-item">
@@ -15,5 +11,4 @@
         <a class="nav-link py-3" href="<?php echo site_url('perfil/papelera_usuarios');?>">Papelera de Reciclaje</a>
       </li>
     </ul>    
-    </div>
     </div>
