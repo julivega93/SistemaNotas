936e4fa7b199e580bb0fcef248ef3dc1fcfb2eca
Julian Vega <julivegapr8@gmail.com>
2020-12-28 16:13:03 -0300

    paneladm

--- application/views/paneladm.php
+++ application/views/paneladm.php
@@ -1,4 +1,8 @@
+<div class="container container-fluid">
 
+
+    
+    
     <div class="bg-dark rounded mt-3" style="height:220px">
     <ul class="nav flex-column">
       <li class="nav-item">
@@ -11,4 +15,5 @@
         <a class="nav-link py-3" href="<?php echo site_url('perfil/papelera_usuarios');?>">Papelera de Reciclaje</a>
       </li>
     </ul>    
+    </div>
     </div>
